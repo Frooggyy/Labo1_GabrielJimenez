f782bfd660e4ec441133e3cae80d235cd18ceac5
Gabriel Jimenez <201839235@edu.clg.qc.ca>
2024-09-10 12:45:54 -0400

    Test
    test

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 import fs from 'fs';
-
+//bello
 function allowAllAnonymousAccess(res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', '*');
